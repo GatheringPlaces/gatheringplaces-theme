883a383f67878f6e6bb8bdb8a066536b476d1941
brodieaustin <brodie.austin@gmail.com>
2018-10-17 23:06:59 -0500

    search page in order

--- src/theme/items/browse.php
+++ src/theme/items/browse.php
@@ -39,9 +39,9 @@ echo head(array('title' => $pageTitle));
           <div class="preview-meta">
             <h2><?php echo link_to_item(metadata('item', array('Dublin Core', 'Title')), array('class' => 'permalink')); ?></h2>
             <?php if ($description = metadata('item', array('Dublin Core', 'Description'), array('snippet' => 140))): ?>
-            <div class="description">
-                <?php echo $description; ?>
-            </div>
+              <div class="description">
+                  <?php echo $description; ?>
+              </div>
             <?php endif; ?>
           </div>
 
--- src/theme/search/index.php
+++ src/theme/search/index.php
@@ -14,33 +14,27 @@ $searchRecordTypes = get_search_record_types();
 
   <section class="landing-page-content">
     <?php if ($total_results): ?>
-      <table id="search-results">
-          <thead>
-              <tr>
-                  <th><?php echo __('Record Type');?></th>
-                  <th><?php echo __('Title');?></th>
-              </tr>
-          </thead>
-          <tbody>
-              <?php $filter = new Zend_Filter_Word_CamelCaseToDash; ?>
-              <?php foreach (loop('search_texts') as $searchText): ?>
-              <?php $record = get_record_by_id($searchText['record_type'], $searchText['record_id']); ?>
-              <?php $recordType = $searchText['record_type']; ?>
-              <?php set_current_record($recordType, $record); ?>
-              <tr class="<?php echo strtolower($filter->filter($recordType)); ?>">
-                  <td>
-                      <?php echo $searchRecordTypes[$recordType]; ?>
-                  </td>
-                  <td>
-                      <?php if ($recordImage = record_image($recordType)): ?>
-                          <?php echo link_to($record, 'show', $recordImage, array('class' => 'image')); ?>
-                      <?php endif; ?>
-                      <a href="<?php echo record_url($record, 'show'); ?>"><?php echo $searchText['title'] ? $searchText['title'] : '[Unknown]'; ?></a>
-                  </td>
-              </tr>
-              <?php endforeach; ?>
-          </tbody>
-      </table>
+      <?php $filter = new Zend_Filter_Word_CamelCaseToDash; ?>
+
+      <ul class="previews">
+        <?php foreach (loop('search_texts') as $searchText): ?>
+          <?php $record = get_record_by_id($searchText['record_type'], $searchText['record_id']); ?>
+          <?php $recordType = $searchText['record_type']; ?>
+          <?php set_current_record($recordType, $record); ?>
+
+          <li class="preview">
+            <div class="preview-img">
+              <?php if ($recordImage = record_image($recordType)): ?>
+                  <?php echo link_to($record, 'show', $recordImage, array('class' => 'image')); ?>
+              <?php endif; ?>
+            </div>
+            <div class="preview-meta">
+              <h2><a href="<?php echo record_url($record, 'show'); ?>"><?php echo $searchText['title'] ? $searchText['title'] : '[Unknown]'; ?></a></h2>
+              <p class="record-type"><?php echo $recordType; ?></p>
+            </div>
+          </li>
+        <?php endforeach; ?>
+      <ul>
       <?php echo pagination_links(); ?>
     <?php else: ?>
     <div id="no-results">
