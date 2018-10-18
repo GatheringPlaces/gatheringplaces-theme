6064a50f6a402fe855006cca71c569d297adbf50
brodieaustin <brodie.austin@gmail.com>
2018-10-18 10:38:30 -0500

    fixed homepage height issue

--- src/theme/index.php
+++ src/theme/index.php
@@ -10,25 +10,23 @@
   }
 ?>
 
-<div class="container">
-  <section class="landing-page homepage">
-    <section class="landing-page-intro" id="page-intro" aria-hidden="false">
-      <button class="collapse-section" aria-expanded="true" aria-controls="page-intro"></button>
-      <p class="site-description"><?php echo get_theme_option('Homepage Tagline'); ?></p>
-      <hr>
-      <div class="site-actions">
-        <?php echo get_theme_option('Homepage Actions'); ?>
-      </div>
-      <hr>
-      <div class="site-meta">
-        <p><?php echo get_theme_option('Homepage Sponsor Text'); ?></p>
-      </div>
-    </section>
-
-    <section class="landing-page-content">
-      <iframe src="<?php echo nl_getExhibitUrl($neatline, "fullscreen")  ?>" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" seamless></iframe>
-    </section>
+<section class="landing-page homepage">
+  <section class="landing-page-intro" id="page-intro" aria-hidden="false">
+    <button class="collapse-section" aria-expanded="true" aria-controls="page-intro"></button>
+    <p class="site-description"><?php echo get_theme_option('Homepage Tagline'); ?></p>
+    <hr>
+    <div class="site-actions">
+      <?php echo get_theme_option('Homepage Actions'); ?>
+    </div>
+    <hr>
+    <div class="site-meta">
+      <p><?php echo get_theme_option('Homepage Sponsor Text'); ?></p>
+    </div>
   </section>
-</div>
+
+  <section class="landing-page-content">
+    <iframe src="<?php echo nl_getExhibitUrl($neatline, "fullscreen")  ?>" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" seamless></iframe>
+  </section>
+</section>
 
 <?php echo foot(); ?>
