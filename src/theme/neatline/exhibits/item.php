428e43b36d6f70205e914344a5e3ba09ebf69de3
brodieaustin <brodie.austin@gmail.com>
2019-01-09 00:20:40 -0600

    made files appear as files in neatline bubbble

--- src/theme/neatline/exhibits/item.php
+++ src/theme/neatline/exhibits/item.php
@@ -2,15 +2,16 @@
   <span class="preview-label">Featured Item</span>
   <?php if (metadata('item', 'has files')): ?>
     <div class="preview-img">
-        <?php echo link_to_item(item_image()); ?>
+        <?php echo files_for_item(array('imageSize' => 'thumbnail'), array('class' => 'item-file')); ?>
     </div>
   <?php endif; ?>
   <div class="preview-meta">
-    <h2><?php echo link_to_item(metadata('item', array('Dublin Core', 'Title')), array('class' => 'permalink')); ?></h2>
+    <h2><?php echo link_to_item(metadata('item', array('Dublin Core', 'Title')), array('class' => 'permalink', 'target' => '_blank')); ?></h2>
     <?php if ($description = metadata('item', array('Dublin Core', 'Description'), array('snippet' => 140))): ?>
       <div class="description">
           <?php echo $description; ?>
       </div>
     <?php endif; ?>
+    <?php echo link_to_item('Learn more', array('class' => 'learn-more', 'target' => '_blank')); ?>
   </div>
 </div>
